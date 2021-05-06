290e05f5f2df7a26935f34788f6d1d3350dfd4f0
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-06 12:14:28 +0100

    set up flask-mail to work with my personal email. Sends email when someone joins the mailing list to welcome them :D

--- api/app.py
+++ api/app.py
@@ -3,10 +3,28 @@ from flask_cors import CORS
 from flask_mail import Mail, Message
 from controllers import users
 
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
 app = Flask(__name__)
 
-CORS(app)
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.getenv("MAIL_USERNAME"),
+    "MAIL_PASSWORD": os.getenv("MAIL_PASSWORD"),
+    "MAIL_DEFAULT_SENDER":'natalie.wuwu@googlemail.com'
+}
 
+app.config.update(mail_settings)
+mail = Mail(app)
+
+
+CORS(app)
+    
 @app.route('/')
 def welcome():
     return "Welcome to Flask backend for Natalie's Travel blog!"
@@ -21,7 +39,10 @@ def users_handler():
         'POST': users.create
     }
     resp, code = fns[request.method](request)
-
+    if request.method == 'POST':
+        msg = Message('HEY WELCOME FRIEND 💖', recipients=[resp["email"]])
+        msg.body = f"Thank you for joining the mailing list, {resp['name']}!"
+        mail.send(msg)
     return jsonify(resp), code
 
 if __name__ == '__main__':
