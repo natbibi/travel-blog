2b2f2c1ff3a1877cc8a3c3712caf32f2bc74b8c4
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-06 12:51:34 +0100

    create JoinForm component that POSTs name and email to api /mailinglist and renders this form on Home page

---
+++ client/src/components/JoinForm/index.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+function JoinForm({subscribe}) {
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post('http://localhost:8000/mailinglist', {email: email, name: name})
+        setName("")
+        setEmail("")
+        subscribe(true)
+    }
+
+    const updateName = e => {
+        const input = e.target.value
+        setName(input)
+    }
+
+    const updateEmail = e => {
+        const input = e.target.value
+        setEmail(input)
+    }
+
+    return (
+        <>
+        <form onSubmit={handleSubmit} role="join-mailing-list">
+            <p>Join my mailing list:</p>
+            <label>Name
+            <input type="text" value={name} onChange={updateName} />
+            </label>
+            <label>Email
+            <input type="text" value={email} onChange={updateEmail} />
+            </label>
+            <button type="submit">Join!</button>
+        </form>
+        </>
+    )
+}
+
+export default JoinForm
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { JoinForm } from '../../components'
 
 const Home = () => {
- 
+
+    const [subscribed, setSubscribed] = useState(false)
+
+    const subscribe = (boolean) => {
+        setSubscribed(boolean)
+    }
+
     return (
         <>
             <header className="intro-container">
                 <h1>Home</h1>
+                {subscribed ? <p>Thank you 💖 Look out for updates from me!</p> : <JoinForm subscribe={subscribe} />}
+
             </header>
         </>
     )
