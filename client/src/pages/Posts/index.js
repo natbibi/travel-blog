fd73e9d294f19b49df75e1b53b28543c12b92d9c
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-26 11:56:01 +0100

    :sparkles: render sample destination data and set up layout

--- client/src/pages/Posts/index.js
+++ client/src/pages/Posts/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Destinations } from '../../components'
+import destinationData from './data'
+
 
 const Posts = () => {
- 
+
+    const [destination, setDestination] = useState(destinationData)
+
+    const renderDestinations = destination.map(d =>
+        <Destinations destinationData={d} key={d.id} />
+    );
+
     return (
         <>
             <header className="intro-container">
                 <h1>Posts!</h1>
             </header>
+
+            <main>
+           {renderDestinations}
+            </main>
         </>
     )
 }
