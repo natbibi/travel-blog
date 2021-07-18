63b11d9c5de60e1f8c68b9922cea2767db964825
natbibi <58271566+natbibi@users.noreply.github.com>
2021-07-18 14:16:33 +0100

    :white_check_mark: update existing test - now passing

--- client/src/pages/NotFound/test.js
+++ client/src/pages/NotFound/test.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import NotFound from '.';
-import { screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history'
+import { Router } from 'react-router-dom';
 
 describe('NotFound', () => {
     test('it renders', () => {
-        renderWithProviders(<NotFound />)
-        const message = screen.getByText(/Oops/)
-        expect(message).toBeInTheDocument()
-    })
+    const history = createMemoryHistory();
+
+    render(
+        <Router history={history}>
+            <NotFound />
+        </Router>
+    );
+
+    expect(screen.getByText(/Oops/i)).toBeInTheDocument()
+    });
 })
